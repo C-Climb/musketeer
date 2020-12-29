7f87ead5263a72134b0dfa5c5b9e21ad0a720b46
C-Climb <deathmarchtepes@gmail.com>
2020-12-29 14:16:54 -0800

    Initial styling & declaration of twitter function.

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from webcrawler import login_pixiv, create_post_pixiv, login_twitter, create_pos
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QLabel
 from PyQt5.QtWidgets import QWidget
+from PyQt5.QtWidgets import QLineEdit
+from PyQt5.QtWidgets import QPushButton
+
 
 def application_startup(app, window):
     window.setWindowTitle("Musketeere")
@@ -16,9 +19,25 @@ def application_startup(app, window):
     sys.exit(app.exec_())
 
 
+def test(*args):
+    print(*args)
+
+
+def twitter_login_form(window):
+    usernameTextbox = QLineEdit(window)
+    usernameTextbox.move(60, 40)
+    passwordTextbox = QLineEdit(window)
+    passwordTextbox.move(60, 80)
+    button = QPushButton(window)
+    button.move(60, 120)
+    button.setText("Submit")
+    button.clicked.connect(lambda: test(usernameTextbox.text(), passwordTextbox.text()))
+
+
 def gui_elements(window):
     helloMsg = QLabel("<body><h1>Hello</h1></body>", parent=window)
     helloMsg.move(60, 15)
+    twitter_login_form(window)
 
 
 def main():
@@ -34,6 +53,6 @@ def main():
 
 if __name__ == "__main__":
     # this but using buttons.
-    asyncio.get_event_loop().run_until_complete(create_post_twitter())
-    asyncio.get_event_loop().run_until_complete(login_twitter())
+    # asyncio.get_event_loop().run_until_complete(create_post_twitter())
+    # asyncio.get_event_loop().run_until_complete(login_twitter())
     main()
